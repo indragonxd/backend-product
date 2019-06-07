5b3956e202747e6a0cab3755509fa01cd6df3316
sherwin <sherwin.marquez@unmsm.edu.pe>
2019-06-07 14:56:31 -0500

    Se realizo los controladores

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,24 +1,66 @@
-import { Controller, Get, Param, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Res, HttpStatus, NotFoundException, Delete, Query, Put } from '@nestjs/common';
 import { CreateProductDto } from './dto/products.dto';
-import { InjectModel } from '@nestjs/mongoose';
+import { ProductService } from './product.service';
+import { json } from 'body-parser';
 
 @Controller('product')
 export class ProductController {
+    /*
+    modo largo ---> mediante injecciones instancia el productService
+    private productService:ProductService
+    constructor(productService:ProductService){
+        this.productService = productService;
+    }
+    */
+    constructor(private productService:ProductService){}
 
-    constructor( ){}
-
-    @Get()
-    getProducts(){}
-
-    @Get(':id')
-    getProduct(@Param('id') id : number){}
-
-    @Post('/create')
-    createProduct(@Res() res ,@Body() productDto:CreateProductDto ){
-
+    @Get('/')
+    async getProducts(@Res() res){
+        const productos = await this.productService.getProducts();
         res.status(HttpStatus.OK).json({
-            mensaje : 'recibido'
-        })
+            mensage : 'envio de productos',
+            value : productos
+        });
+        
     }
 
+    @Get('/:id')
+    async getProduct(@Res() res, @Param('id') id : number){
+        const product = await this.productService.getProduct(id);
+        if(!product) throw new NotFoundException('El id no existe');
+        res.status(HttpStatus.OK).json({
+            mensage : 'envio producto',
+            value : product
+        });
+    }
+
+    @Post('/create')
+    //@Res() con este decorador se le puede responder al cliente
+    async createProduct(@Res() res ,@Body() productDto:CreateProductDto ){
+        const newProduct = await this.productService.createProduct(productDto);
+        res.status(HttpStatus.OK).json({
+            mensaje : 'Producto creado',
+            value : newProduct
+        })
+    }
+    //se pudo hacer /delete/:id
+    //la peticion ahora es ../delete?productID=1
+    @Delete('/delete')
+    deleteProduct(@Res() res , @Query('productID') id :number){
+        const product = this.productService.deleteProduct(id);
+        if(!product) throw new NotFoundException('El id no existe');
+        res.status(HttpStatus.OK).json({
+            mensage : 'se elimino',
+            value : product
+        });
+    }
+    @Put('/update')
+    updateProduct(@Res() res ,@Body() productDto:CreateProductDto , @Query('productID') id :number){
+        const updtProduct = this.productService.updateProduct(id,productDto);
+        if(!updtProduct)throw new NotFoundException('No se encontro el id');
+        res.status(HttpStatus.OK).json({
+            mensage : 'se actualizo el producto',
+            value : updtProduct
+        });
+    }
 }
